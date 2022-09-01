555ca60fc33fc10482f12c55e09d86bd8858a6b2
Tim_P <ruimtesonde@gmail.com>
2022-09-02 00:12:27 +0200

    Add project files.

---
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<string> get(int id)
+    {
+        return "Les Jackson";
+    }
+}
---
+++ test/SimpleAPI.Test/ValuesControllerTest.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleAPI.Test;
+public class ValuesControllerTest
+{
+    private ValuesController _controller;
+
+    public ValuesControllerTest()
+    {
+        _controller = new ValuesController();
+    }
+
+    
+
+    [Fact]
+    public void GetReturnsMyName()
+    {
+        var returnValue = _controller.get(1);
+
+        Assert.Equal("Les Jackson", returnValue.Value);
+    }
+}
